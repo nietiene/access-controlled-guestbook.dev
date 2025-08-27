6cbc4a8ffbd5337d529638a11c9cf3b0fee1ab52
nietiene <niyomugaboetiene53@gmail.com>
2025-08-27 15:25:58 -0700

    commit of modifiers in solidity 😎 😎

---
+++ contracts/modifiers.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+// modifier are reusable piece of code that can change behavior of the function
+// usercase: RBAC, input validation, check codde before running them
+// syntax
+// modifier modifierName {
+// some logic
+// _; codes before _ runs before function runs, codes after _ runs after function runs
+// }
+contract ModifierExample {
+    // 1. RBAC: for this example only contract owner should call function named setData
+  address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+     modifier onlyOwner {
+        require(msg.sender == owner, "Only owner allowed");
+        _;
+     }
+
+     string public data;
+     function setData (string memory _data) public onlyOwner {
+         data = _data;
+     }
+
+} 
